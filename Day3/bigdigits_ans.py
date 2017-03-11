8cbd9aece8d434ef158e02f2b28a2482d3ba14e5
Tara Vulpes <martynyuk.darya@gmail.com>
2017-03-11 17:55:38 +0200

    The bigdigits task from the Summerfield book.

---
+++ Day3/bigdigits_ans.py
@@ -0,0 +1,54 @@
+"""bigdigits_ans.py
+
+    Prints the input in form of big numbers created from the input digits.
+    Input is provided in for of number as argument when the program is called
+    Returns big number in 7 rows created from the input digits
+"""
+
+__author__ = "maydee"
+
+
+import sys
+
+
+Zero = ["  ***  ",
+        " *   * ",
+        "*     *",
+        "*     *",
+        "*     *",
+        " *   * ",
+        "  ***  "]
+One = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two = [" *** ", "*   *", "*  * ", "  *  ", " *   ", "*    ", "*****"]
+Three = [" *** ", "*   *", "    *", "  ** ", "    *", "*   *", " *** "]
+Four = ["   *  ", "  **  ", " * *  ", "*  *  ", "******", "   *  ",
+        "   *  "]
+Five = ["*****", "*    ", "*    ", " *** ", "    *", "*   *", " *** "]
+Six = [" *** ", "*    ", "*    ", "**** ", "*   *", "*   *", " *** "]
+Seven = ["*****", "    *", "   * ", "  *  ", " *   ", "*    ", "*    "]
+Eight = [" *** ", "*   *", "*   *", " *** ", "*   *", "*   *", " *** "]
+Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
+try:
+    digits = sys.argv[1]
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(digits):
+            number = int(digits[column])
+            digit = Digits[number]
+            for point in digit[row]:
+                if point == '*':
+                    point = str(number)
+                line += point
+            line += " "
+            column += 1
+        print(line)
+        row += 1
+except IndexError:
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits) 
