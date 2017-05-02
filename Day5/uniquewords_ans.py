0945b74ae75b542c34d5d128a3d78b753973579d
Tara Vulpes <martynyuk.darya@gmail.com>
2017-05-02 16:28:22 +0300

    external_sites and uniquewords done from Summerfield. generate_username base.

---
+++ Day5/uniquewords_ans.py
@@ -0,0 +1,27 @@
+import sys
+import collections
+import string
+
+"""csv2html1_ans.py
+
+    Transform the CSV file into HTML file.
+
+"""
+
+__author__ = "maydee"
+
+
+def sort_by_frequency(pair):
+    return pair[1]
+
+words = collections.defaultdict(int)
+strip = string.whitespace + string.punctuation + string.digits + "\"'"
+for filename in sys.argv[1:]:
+    with open(filename) as file:
+        for line in file:
+            for word in line.lower().split():
+                word = word.strip(strip)
+                if len(word) > 2:
+                    words[word] += 1
+for word, count in sorted(words.items(), key=sort_by_frequency):
+    print("'{0}' occurs {1} times".format(word, count))
